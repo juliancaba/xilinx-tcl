b61ad117e53a7914f90743e69ee64a109327d631
julian.caba <julian.caba@uclm.es>
2019-12-29 23:50:50 +0100

    Updated example

--- examples/bram/sw.tcl
+++ examples/bram/sw.tcl
@@ -3,9 +3,10 @@ source /opt/xilinx-tcl/build/project.tcl
 set board [lindex $argv 0]
 project::setBoard $board
 
-set workspace [pwd]/[lindex $argv 1].sdk
+set prj [lindex $argv 1]
+set workspace [pwd]/$prj.sdk
 project::setWorkspace $workspace
-project::setProjectName test
+project::setProjectName $prj
 
 
 
